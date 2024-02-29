4306a86fa2e1b3ee06c7eb9251789c5f9888077c
João Vítor Pamponet <100170271+JV1T0R@users.noreply.github.com>
2024-02-28 21:25:39 -0300

    Delete functions directory

--- functions/concluidor_de_tarefas.py
+++
@@ -1,8 +0,0 @@
-def concluir_tarefa(tarefas):
-    selecionar_tarefa = input("Digite o Título da Tarefa para Concluir: ")
-    for titulo, tarefa in tarefas.items():
-        if 'Titulo' in tarefa and selecionar_tarefa == tarefa['Titulo']:
-            del tarefas[titulo]
-            print("Parabéns. Tarefa Concluída!")
-            return
-    print("Tarefa Não Encontrada.")
